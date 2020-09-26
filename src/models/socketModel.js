8cc3d7ed72e7d7f7afb6d99eb40bf814f939907a
Daniel Subiabre <subiabrewd@gmail.com>
2020-09-26 19:26:28 +0200

    Add socket.io socket model for database storage

---
+++ src/models/socketModel.js
@@ -0,0 +1,26 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:', {logging: false});
+
+const Socket = sequelize.define('Socket', {
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  userAgent: {
+      type: DataTypes.JSON,
+      allowNull: true
+  },
+  userAgentOriginal: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  connectedAt: {
+      type: DataTypes.TIME,
+      allowNull: false
+  }
+});
+
+Socket.sync({ force: true });
+
+module.exports = Socket;
