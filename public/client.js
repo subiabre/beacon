93dff6fad3fc868344ca51180c48465a9c0a9a6a
Daniel Subiabre <subiabrewd@gmail.com>
2020-09-27 12:24:05 +0200

    Refactor client code

--- public/client.js
+++ public/client.js
@@ -1,16 +1,24 @@
-const $events = document.getElementById('events');
+const socketList = document.getElementById('sockets');
 
-const newItem = (content) => {
-    const item = document.createElement('li');
-    item.innerText = content;
+const newSocket = (socket) => {
+    let item = document.createElement('li');
+    
+    item.setAttribute('id', 'socket' + socket.id);
+    item.setAttribute('class', 'socket');
+    item.innerText = `${socket.userAgent.name} @ ${socket.userAgent.os} ${socket.userAgent.device_type}`;
     return item;
 };
+
+const updateSocketList = (list) => {    
+    socketList.innerHTML = '';
+    list.forEach(socket => {
+        let item = newSocket(socket);
+        socketList.appendChild(item);
+    });
+}
 
 const socket = io();
 
 socket.on('socket:update', (list) => {
-    $events.innerHTML = '';
-    list.forEach(item => {
-    $events.appendChild(newItem(`${item.id}`));
-    });
+    updateSocketList(list);
 });
