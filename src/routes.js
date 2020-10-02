816c6ddeb52c5d59eae845cdfb0abae5605f1304
Daniel Subiabre <subiabrewd@gmail.com>
2020-10-02 16:29:42 +0200

    Start api

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 "use strict";
 
 const express = require('express');
+const Song = require('./models/songModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
+});
+
+router.get('/songs/last', async (req, res) => {
+    let songs = await Song.findAll({
+        limit: 4,
+        order: [[ 'createdAt', 'DESC' ]]
+    });
+
+    console.log(songs);
+    res.send({data: songs});
 });
 
 module.exports = router;
